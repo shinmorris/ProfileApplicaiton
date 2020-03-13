efc0a3a8595e30e54d13cb997a219ea10ae90351
shinmorris <shin3298s@gmail.com>
2020-03-13 13:11:15 +0900

    routeジ

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
   root 'profiles#index'
   resources :messages, only: [:index, :create, :update, :destroy]
   resources :users,    only: [:edit, :update]
